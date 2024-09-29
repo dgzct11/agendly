04f37675723d8fa5d88f4e9261b51bb29259c977
Alfred <alfredtapiaratana@gmail.com>
2024-09-28 19:41:37 -0500

    Added Upload Functionality to Upload Component

--- agendly/src/app/content/upload.tsx
+++ agendly/src/app/content/upload.tsx
@@ -1,4 +1,52 @@
+'use client'
+import { useRef, useState } from "react";
+
+
 export default function Upload() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [dragging, setDragging] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+
+  const handleClick = () => {
+    if (fileInputRef.current){
+      fileInputRef.current.click();
+    }
+    else{
+      console.error("File input ref is null");
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0] || null;
+    setSelectedFile(file);
+    console.log(selectedFile)
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(true);
+    console.log("Drag over")
+  };
+
+  const handleDragLeave = () => {
+    setDragging(false);
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(false);
+
+    const file = e.dataTransfer.files[0];
+    
+    // Check if the dropped file is a PDF
+    if (file && file.type === 'application/pdf') {
+      setSelectedFile(file);
+    } else {
+      setSelectedFile(null);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-28 w-screen h-screen bg-gradient-to-br from-gray-100 to-gray-300 items-center overflow-hidden">
       <div className="flex flex-col mt-10 h-40 w-5/6">
@@ -10,14 +58,21 @@ export default function Upload() {
         </h3>
         
       </div>
-      <div className="items-center shadow-lg bg-gray-300 flex h-1/2 w-4/6 max-w-3/6 rounded-sm border border-opacity-75 border-4 border-gray-400 border-solid">
+      <div 
+        className={`items-center shadow-lg flex h-1/2 w-4/6 max-w-3/6 rounded-sm border border-opacity-75 border-4 border-gray-500 border-solid ${dragging ? 'bg-gray-300' : 'bg-gray-200'}`}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}>
         <div className="m-auto">
-          <button className="bg-blue-400 hover:bg-blue-500 shadow-md rounded-xl pt-8 pb-8 pl-16 pr-16">
+          <button className="bg-blue-400 hover:bg-blue-500 shadow-md rounded-xl pt-8 pb-8 pl-16 pr-16" 
+          onClick={handleClick}>
             Upload PDF Files
           </button>
           <p className="text-center text-sm text-blue-900 opacity-50">
             or drop PDFs here
           </p>
+          <input type="file" ref={fileInputRef} onChange={handleFileChange} className="hidden">
+          </input>
         </div>
       </div>
     </div>
