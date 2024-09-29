851825667eb583095ad5f78d0c8b3c20bb6df945
mafra23 <mofoFranco23@gmail.com>
2024-09-28 21:36:48 -0500

    Changed results page and changed shadecn theme

---
+++ agendly/src/app/results/page.tsx
@@ -0,0 +1,10 @@
+import Results from "@/app/content/components/Results"
+import Navbar from "../content/components/Navbar";
+
+const ResultsPage = () => {
+    
+    return( <Results/> );
+
+};
+
+export default ResultsPage;
